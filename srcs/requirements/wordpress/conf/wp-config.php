bf8c912613f0b7377363fb17aea82b7bf5f89e7f
user <user@inception.myguest.virtualbox.org>
2024-05-20 17:12:58 +0100

    all

---
+++ srcs/requirements/wordpress/conf/wp-config.php
@@ -0,0 +1,94 @@
+<?php
+/**
+* As configurações básicas do WordPress
+*
+* O script de criação wp-config.php usa esse arquivo durante a instalação.
+* Você não precisa usar o site, você pode copiar este arquivo
+* para "wp-config.php" e preencher os valores.
+*
+* Este arquivo contém as seguintes configurações:
+*
+* * Configurações do MySQL
+* * Chaves secretas
+* * Prefixo do banco de dados
+* * ABSPATH
+*
+* @link https://codex.wordpress.org/pt-br:Editando_wp-config.php
+*
+* @package WordPress
+*/
+
+// ** Configurações do MySQL - Você pode pegar estas informações com o serviço de hospedagem ** //
+/** O nome do banco de dados do WordPress */
+define('DB_NAME', getenv('DB'));
+
+/** Usuário do banco de dados MySQL */
+define('DB_USER', getenv('DB_USER'));
+
+/** Senha do banco de dados MySQL */
+define('DB_PASSWORD', getenv('DB_USER_PSWD'));
+
+/** Nome do host do MySQL */
+define('DB_HOST', getenv('DB_HOST'));
+
+/** Charset do banco de dados a ser usado na criação das tabelas. */
+define('DB_CHARSET', 'utf8');
+
+/** O tipo de Collate do banco de dados. Não altere isso se tiver dúvidas. */
+define('DB_COLLATE', '');
+
+/**#@+
+* Chaves únicas de autenticação e salts.
+*
+* Altere cada chave para um frase única!
+* Você pode gerá-las
+* usando o {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org
+* secret-key service}
+* Você pode alterá-las a qualquer momento para invalidar quaisquer
+* cookies existentes. Isto irá forçar todos os
+* usuários a fazerem login novamente.
+*
+* @since 2.6.0
+*/
+define('AUTH_KEY',         getenv('AUTH_KEY'));
+define('SECURE_AUTH_KEY',  getenv('SECURE_AUTH_KEY'));
+define('LOGGED_IN_KEY',    getenv('LOGGED_IN_KEY'));
+define('NONCE_KEY',        getenv('NONCE_KEY'));
+define('AUTH_SALT',        getenv('AUTH_SALT'));
+define('SECURE_AUTH_SALT', getenv('SECURE_AUTH_SALT'));
+define('LOGGED_IN_SALT',   getenv('LOGGED_IN_SALT'));
+define('NONCE_SALT',       getenv('NONCE_SALT'));
+
+/**#@-*/
+
+/**
+* Prefixo da tabela do banco de dados do WordPress.
+*
+* Você pode ter várias instalações em um único banco de dados se você der
+* um prefixo único para cada um. Somente números, letras e sublinhados!
+*/
+$table_prefix  = 'wp_';
+
+/**
+* Para desenvolvedores: Modo de debug do WordPress.
+*
+* Altere isto para true para ativar a exibição de avisos
+* durante o desenvolvimento. É altamente recomendável que os
+* desenvolvedores de plugins e temas usem o WP_DEBUG
+* em seus ambientes de desenvolvimento.
+*
+* Para informações sobre outras constantes que podem ser utilizadas
+* para depuração, visite o Codex.
+*
+* @link https://codex.wordpress.org/pt-br:Depura%C3%A7%C3%A3o_no_WordPress
+*/
+define('WP_DEBUG', false);
+
+/* Isto é tudo, pode parar de editar! 🙂 */
+
+/** Caminho absoluto para o diretório WordPress. */
+if ( !defined('ABSPATH') )
+   define('ABSPATH', dirname(__FILE__) . '/');
+
+/** Configura as variáveis e arquivos do WordPress. */
+require_once(ABSPATH . 'wp-settings.php');
