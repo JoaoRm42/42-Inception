32e07c9c3e73cec5ae7a8f93c3ff7d691c6b4c85
joaoped2 <joaoped2@Inception42.myguest.virtualbox.org>
2024-05-31 17:51:53 +0100

    Cleaned and commented some files

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -1,10 +1,30 @@
 #!/bin/bash
 
-sleep 5s
+#Using sleep so it has a small delay and we let mariadb
+#fully initialize to prevent issues on startup
+sleep 2s
+
+#Making sure we have inside the correct installation
+#folder so we can install everything inside it
 cd /var/www/wordpress
+
+#Check if there's index file inside our wordpress folder
+#if not we download it and install everything to get it
+#working properly
 if [ ! -f "/var/www/wordpress/index.php" ]; then
+	#Downloads WordPress Files
     wp core download --allow-root
-    wp config create --allow-root --dbname=$DB --dbuser=$DB_USER --dbpass=$DB_USER_PSWD --dbhost=$DB_HOST --dbcharset="utf8" --dbcollate="utf8_general_ci"
+
+	#Created wp-config.php
+    wp config create --allow-root \
+	--dbname=$DB \
+	--dbuser=$DB_USER \
+	--dbpass=$DB_USER_PSWD \
+	--dbhost=$DB_HOST \
+	--dbcharset="utf8" \
+	--dbcollate="utf8_general_ci"
+
+	#Install of the webpage
     wp core install --allow-root \
 	--url=https://$DOMAIN \
 	--title=$TITLE_WP \
@@ -12,6 +32,7 @@ if [ ! -f "/var/www/wordpress/index.php" ]; then
 	--admin_password=$SPUSR_PWD_WP \
 	--admin_email=$SPUSR_EMAIL_WP \
 
+	#Creation of the user
 	wp user create --allow-root \
 		$USR_WP \
 		$USR_EMAIL_WP \
@@ -19,11 +40,7 @@ if [ ! -f "/var/www/wordpress/index.php" ]; then
 		--user_pass=$USR_PWD_WP
 fi
 
-sed -i "s/memory_limit = .*/memory_limit = 256M/" /etc/php/7.4/fpm/php.ini
-sed -i "s/upload_max_filesize = .*/upload_max_filesize = 128M/" /etc/php/7.4/fpm/php.ini
-sed -i "s/zlib.output_compression = .*/zlib.output_compression = on/" /etc/php/7.4/fpm/php.ini
-sed -i "s/max_execution_time = .*/max_execution_time = 18000/" /etc/php/7.4/fpm/php.ini
-
+#Restart of php7.4-fpm service
 service php7.4-fpm start 
 service php7.4-fpm stop
 /usr/sbin/php-fpm7.4 -F -y /etc/php/7.4/fpm/php-fpm.conf
